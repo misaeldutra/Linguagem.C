f5d400c40f5e7a2d0c0c7c0de17d36c341145431
Misael Dutra <misaeldutra.dev@gmail.com>
2024-11-07 16:20:50 -0300

    questão 5

--- exercicio-algoritmos/questao-5.c
+++ exercicio-algoritmos/questao-5.c
@@ -2,17 +2,26 @@
 #include <stdlib.h>
 
 int main() {
-    float custo, cfa, percent, imposto;
+    float custo, cfa, distrib, imposto;
 
     
     cfa= 0;
-    percent= 0.28;
+    distrib= 0.28;
     imposto= 0.45; 
 
     printf("Digite o custo de fabrica de um carro: " );
     scanf("%f", &cfa);
 
+    distrib= cfa*0.28;
 
+    custo= cfa +(cfa*imposto) + distrib;
+
+    printf("O custo final ao consumidor foi de %.f\n" , custo);
+
+    system("pause");
     return(0);
+
+
+   
     
 }

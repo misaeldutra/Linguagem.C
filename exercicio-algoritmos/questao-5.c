8ef17942b7fa8636106dcf8e42d36f356edddb0c
Misael Dutra <misaeldutra.dev@gmail.com>
2024-11-07 15:57:31 -0300

    testando questão 5

---
+++ exercicio-algoritmos/questao-5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float custo, cfa, percent, imposto;
+
+    
+    cfa= 0;
+    percent= 0.28;
+    imposto= 0.45; 
+
+    printf("Digite o custo de fabrica de um carro: " );
+    scanf("%f", &cfa);
+
+
+    return(0);
+    
+}

e1aa8ca6349a8c4fbe27c66f17ff33b9d0901aa0
Misael Dutra <misaeldutra.dev@gmail.com>
2024-11-14 12:56:01 -0300

    questão 6

--- Exercícios-Algoritmos/Lista 2 - Seleção/questao-6.c
+++ Exercícios-Algoritmos/Lista 2 - Seleção/questao-6.c
@@ -3,27 +3,33 @@
 #include <conio.h>
 #include <locale.h>
 
-int main() {
+ main() {
     float saldo, debito, credito, saldoatual;
 
     printf("Qual o saldo? ");
-    scanf("%f", saldo);
+    scanf("%f", &saldo);
 
     printf("Qual o debito? ");
-    scanf("%f", debito);
+    scanf("%f", &debito);
 
     printf("Qual o credito? ");
-    scanf("%f", credito);
-
-    if(saldo>= 0){
-        printf("Saldo Positivo!");
-    }else{
-        printf("Saldo Negativo!");
-    }
+    scanf("%f", &credito);
 
     saldoatual= saldo - debito + credito;
 
-    printf("O saldo atual do cliente foi %.2f", saldoatual);
+    printf("O saldo atual = %.2f \n", saldoatual);
+
+    if(saldoatual>= 0){
+        printf("Saldo Positivo!\n");
+        
+    }else{
+        printf("Saldo Negativo!\n");
+        saldoatual= 0;
+    }
+
+    
+
+    printf("O saldo atual do cliente = %.2f\n", saldoatual);
 
 system("pause");
 return(0);
---
+++ Exercícios-Algoritmos/Lista 2 - Seleção/questao-7.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <locale.h>
+
+int main() {
+
+}

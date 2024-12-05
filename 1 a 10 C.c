51d4aadc34573578fa62a81705606c65239c74dd
Misael Dutra <misaeldutra.dev@gmail.com>
2024-12-05 11:24:16 -0300

    Estruturas de Repetição

---
+++ 1 a 10 C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i;
+	
+	i = 1;
+	
+	while(i <= 10){
+		//comandos
+		printf("%d", i);
+		i = i + 1; //i++
+	}
+}
---
+++ 1 a 10 WHILE.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i;
+	
+	i = 1;
+	
+	do{
+		printf("%d", i);
+		i = i + 1; //i++;
+		
+	}while(i <= 10);
+	
+
+}

279071549501a2eadb14831e1c7259596bd4aa98
Misael Dutra <misaeldutra.dev@gmail.com>
2025-01-27 08:07:00 -0300

    inicio Questão 5

--- Lista de exercícios - VETORES/Questao5.c
+++ Lista de exercícios - VETORES/Questao5.c
@@ -2,12 +2,33 @@
 #include <stdlib.h>
 #include <locale.h>
 
+	
 int main(){
 	setlocale(LC_ALL,"Portuguese");
 	
 	// Escrever um algoritmo que le um vetor K(15) e o escreve. Crie, a seguir, um vetor P, que contenha todos os números primos K. Escreva o vetor P.
 	
+	int K[15], P[15];
+	int i, j, numero;
+	int primo;
 	
+	if(numero < 2) return 0;
+	
+	for(i = 2; i * i <= numero; i++){
+		if(numero % i == 0) return 0;
+	}
+	return 1; // logo, é primo;
+	
+	printf("\n Vetor K: ");
+	for (i = 0; i < 15; i++){
+		printf("%d", K[i]);
+	}
+	
+	printf("\n P(primo): ");
+	
+	for (i = 0; i < j; i++){
+		printf("%d", P[i]);
+	}
 	
 	return 0;
 }
